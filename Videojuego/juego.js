f22c48595a07c667d784c5a98d3c3bc8d6a4ece3
prlongoria <Prlongoria@gmail.com>
2022-05-20 08:30:46 +0200

    actualizado

---
+++ Videojuego/juego.js
@@ -0,0 +1,9 @@
+
+
+function separateWords (words){
+    let word = words.split (' ');
+    console.log (word[0]);
+    console.log (word[1]);
+}
+separateWords ('catch use');
+separateWords ('go inspect');
