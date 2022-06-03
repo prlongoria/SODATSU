8ced0e1bb61a98c3358212d1fe3afeb3df5986e7
prlongoria <Prlongoria@gmail.com>
2022-06-03 07:15:38 +0200

    Añado crear lista que cambia de colores y ejercicio en parejas para ordenar array desconocido, aún sin terminar

---
+++ Trabajos js/crear_lista_html/lista.js
@@ -0,0 +1,25 @@
+let colors = document.getElementsByClassName("colors");
+const boton = document.getElementById("boton");
+colors = Array.from(colors);
+boton.onclick = changeColor;
+function changeColor (){
+    colors.forEach(myFunction);
+    function myFunction (element){        
+        element.style.color = element.innerHTML.toLowerCase();
+    };
+}
+
+
+//Otra manera: (menos clara para el que lo lea)
+/*let colors = document.getElementsByClassName("colors");
+const boton = document.getElementById("boton");
+colors = Array.from(colors);
+boton.onclick = changeColor;
+function changeColor (){
+    colors.forEach(element => {
+        
+        element.style.color = element.innerHTML.toLowerCase();
+    });
+
+}
+console.dir(colors);*/
